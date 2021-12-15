38d9a0725d0711ab5a47a4cd9dc8c5653383474c
awesomemoder316 <thealtfulthing@tutanota.com>
2021-12-14 22:10:22 -0800

    Fix relocation.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ConfigureShadowRelocation
+
 import java.io.FileOutputStream
 import java.util.*
 
 plugins {
-    id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("com.github.johnrengelman.shadow") version "7.1.1"
     java
 }
 
@@ -12,8 +14,39 @@ version = "1.0.0"
 
 
 allprojects {
+    apply {
+        plugin("com.github.johnrengelman.shadow")
+        plugin("java")
+    }
+
     repositories {
         mavenCentral()
+    }
+
+    dependencies {
+        annotationProcessor("com.google.dagger:dagger-compiler:2.40.5")
+        implementation("com.diogonunes:JColor:5.2.0")
+        implementation("com.google.dagger:dagger:2.40.5")
+        implementation("com.squareup.moshi:moshi:1.13.0")
+        implementation("com.squareup.okhttp3:okhttp:4.9.3")
+        implementation("com.squareup.okhttp3:okhttp-urlconnection:4.9.3")
+    }
+
+    tasks {
+
+        compileJava {
+            options.release.set(11)
+        }
+
+        register<ConfigureShadowRelocation>("relocateShadowJar") {
+            target = shadowJar.get()
+            prefix = "${rootProject.group}.dependencies"
+        }
+
+        shadowJar {
+            archiveFileName.set(rootProject.name + "-" + rootProject.version + ".jar")
+            dependsOn("relocateShadowJar")
+        }
     }
 }
 
@@ -39,10 +72,11 @@ tasks {
             versionProperties.setProperty("ModgetManifestVersion", supportedManifestVersion)
             versionProperties.store(
                 fileOutputStream,
-                "A file that contains ModgetCE's version, and the targeted manifest version"
+                "A file that contains ModgetCE's version, and the targeted manifest version."
             )
 
             fileOutputStream.close()
         }
     }
+
 }
