259fcef33ffe834ddf364572958fbd919fffe20d
awesomemoder316 <thealtfulthing@tutanota.com>
2021-12-14 17:45:01 -0800

    Add dependency on Moshi (Json parser).

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     annotationProcessor("com.google.dagger:dagger-compiler:2.40.5")
     implementation("com.diogonunes:JColor:5.2.0")
     implementation("com.google.dagger:dagger:2.40.5")
+    implementation("com.squareup.moshi:moshi:1.13.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
     implementation("com.squareup.okhttp3:okhttp-urlconnection:4.9.3")
 }
@@ -56,6 +57,7 @@ tasks {
 
         relocate("com.diogonunes:JColor", "${rootProject.group}.dependencies.jcolor")
         relocate("com.google.dagger", "${rootProject.group}.dependencies.dagger")
+        relocate("com.squareup.moshi", "${rootProject.group}.dependencies.moshi")
         relocate("com.squareup.okhttp3", "${rootProject.group}.dependencies.okhttp3")
 
     }
--- src/main/java/io/github/awesomemoder316/modgetcreate/dependencies/DependencyModule.java
+++ src/main/java/io/github/awesomemoder316/modgetcreate/dependencies/DependencyModule.java
@@ -1,11 +1,19 @@
 package io.github.awesomemoder316.modgetcreate.dependencies;
 
+import com.squareup.moshi.Moshi;
 import dagger.Binds;
 import dagger.Module;
 import okhttp3.OkHttpClient;
 
+import javax.inject.Singleton;
+
 @Module
 public interface DependencyModule {
     @Binds
-    OkHttpClient getOkhttpClient(OkHttpClient impl);
+    @Singleton
+    Moshi moshi(Moshi impl);
+
+    @Binds
+    @Singleton
+    OkHttpClient okhttpClient(OkHttpClient impl);
 }
