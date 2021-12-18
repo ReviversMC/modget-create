f9137682def3a5bba85a3c27da049d6805afc928
awesomemoder316 <thealtfulthing@tutanota.com>
2021-12-17 21:00:49 -0800

    Add yamlmapper, and annotate two different ObjectMapper with "json" and "yaml".

--- modget-create-core/src/main/java/com/github/reviversmc/modget/create/dependencies/DependencyModule.java
+++ modget-create-core/src/main/java/com/github/reviversmc/modget/create/dependencies/DependencyModule.java
@@ -1,15 +1,19 @@
 package com.github.reviversmc.modget.create.dependencies;
 
+import javax.inject.Named;
 import javax.inject.Singleton;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import dagger.Module;
 import dagger.Provides;
 import okhttp3.OkHttpClient;
 
 @Module
 public interface DependencyModule {
+
+    @Named("json")
     @Provides
     @Singleton
     static ObjectMapper objectMapper() {
@@ -20,5 +24,12 @@ public interface DependencyModule {
     @Singleton
     static OkHttpClient okhttpClient() {
         return new OkHttpClient.Builder().build();
+    }
+
+    @Named("yaml")
+    @Provides
+    @Singleton
+    static ObjectMapper yamlMapper() {
+        return new ObjectMapper(new YAMLFactory());
     }
 }
