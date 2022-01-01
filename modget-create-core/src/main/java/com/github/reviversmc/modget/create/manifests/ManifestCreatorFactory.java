5bc15d1c981e38d750e6389fc61458bbbe6cfe51
CloudOn9 <thealtfulthing@tutanota.com>
2022-01-01 13:52:09 -0800

    Add more constructor arguments that need to be filled by user.

--- modget-create-core/src/main/java/com/github/reviversmc/modget/create/manifests/ManifestCreatorFactory.java
+++ modget-create-core/src/main/java/com/github/reviversmc/modget/create/manifests/ManifestCreatorFactory.java
@@ -3,8 +3,15 @@ package com.github.reviversmc.modget.create.manifests;
 import dagger.assisted.Assisted;
 import dagger.assisted.AssistedFactory;
 
+import java.util.List;
+
 @AssistedFactory
 public interface ManifestCreatorFactory {
-    V4ManifestCreator create(@Assisted("authToken") String authToken,
-                             @Assisted("modJarPath") String modJarPath);
+    V4ManifestCreator create(
+            List<String> updateAlternatives,
+            @Assisted("authToken") String authToken,
+            @Assisted("curseId") String curseforgeId,
+            @Assisted("modJarPath") String modJarPath,
+            @Assisted("modrinthId") String modrinthId
+    );
 }
