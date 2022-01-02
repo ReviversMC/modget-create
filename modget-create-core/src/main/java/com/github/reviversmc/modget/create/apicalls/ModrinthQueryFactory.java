73efe6942a2e3ad2bd0c85d3cb2714796dca708c
CloudOn9 <thealtfulthing@tutanota.com>
2022-01-01 21:59:52 -0800

    Fix annotations.

--- modget-create-core/src/main/java/com/github/reviversmc/modget/create/apicalls/ModrinthQueryFactory.java
+++ modget-create-core/src/main/java/com/github/reviversmc/modget/create/apicalls/ModrinthQueryFactory.java
@@ -1,5 +1,8 @@
 package com.github.reviversmc.modget.create.apicalls;
 
+import dagger.assisted.AssistedFactory;
+
+@AssistedFactory
 public interface ModrinthQueryFactory {
     ModrinthV1Query create(String projectId);
 }
